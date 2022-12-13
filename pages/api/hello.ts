56fd3a0c56b11f56859bc0be6449e97a20152ef5
srikar-samudrala <srikar@coto.world>
2022-12-13 13:54:11 +0530

    add catch

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,29 +2,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 
-type Data = {
-  updated: boolean;
-};
+type Data =
+  | {
+      updated: boolean;
+    }
+  | {
+      error: string;
+    };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const animals = await fetch(
-    'https://e7vk8w4f.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22animal%22%5D'
-  ).then((res) => res.json());
+  try {
+    const animals = await fetch(
+      'https://e7vk8w4f.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22animal%22%5D'
+    ).then((res) => res.json());
 
-  const str = animals.result.reduce((acc: string, data: any) => {
-    acc = acc + data.name;
-    return acc;
-  }, '');
+    const str = animals.result.reduce((acc: string, data: any) => {
+      acc = acc + data.name;
+      return acc;
+    }, '');
 
-  let sitemapContent = `<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    let sitemapContent = `<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
   <loc>https://s3.coto.com/careers/sitemap.xml/${str || 'hello'}</loc>
   </sitemap>
   </sitemapindex>`;
 
-  fs.writeFileSync('./public/sitemap.xml', sitemapContent);
-  res.status(200).json({ updated: true });
+    fs.writeFileSync('./public/sitemap.xml', sitemapContent);
+    res.status(200).json({ updated: true });
+  } catch (error: any) {
+    res.status(500).json({ error: (error as Error).message });
+  }
 }
